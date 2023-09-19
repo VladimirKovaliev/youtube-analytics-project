afe3a237d4bf0727596a95110deeaceb538e0dbb
VladimirKovaliev <kovaliev.v@list.ru>
2023-09-19 16:29:51 +0300

    fixes

--- homework-3/main.py
+++ homework-3/main.py
@@ -1,9 +1,11 @@
 from src.channel import Channel
-
+import os
 if __name__ == '__main__':
     # Создаем два экземпляра класса
-    moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-    highload = Channel('UCwHL6WHUarjGfUM_586me8w')
+    channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'
+    api_key = os.getenv('YT_API_KEY')
+    moscowpython = Channel(channel_id, api_key)
+    highload = Channel('UCwHL6WHUarjGfUM_586me8w', api_key)
 
     # Используем различные магические методы
     print(moscowpython)  # 'MoscowPython (https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A)'
